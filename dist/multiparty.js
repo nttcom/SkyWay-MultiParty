e9deff2e04ac137e91d364dd5817223ba41315f1
r-konishi <r.konishi@developer.mebro.co.jp>
2016-01-25 10:53:24 +0900

    startPollingConnections_ 内の変数参照の変更

--- dist/multiparty.js
+++ dist/multiparty.js
@@ -1346,7 +1346,7 @@ new function() {
           if(removeId) {
             self.removePeer(peer_id);
           } else {
-            var peer = multiparty.peers[peer_id];
+            var peer = self.peers[peer_id];
             var reconnect = {
               video: peer.call?!peer.call.open:false,
               screen: peer.screen_sender?!peer.screen_sender.open:false,
